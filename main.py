1e4453fee01be55be6060e435eb3a88708a0f92e
ex1ndle1 <muzaffarilzomjonov@gmail.com>
2026-01-28 20:50:10 +0500

    .

--- main.py
+++ main.py
@@ -16,11 +16,69 @@ class LinkedList:
             return
         
         while current:
-            print(current.data)
+            print(current.data )
             current = current.next
 
         print('None')
 my_list = LinkedList()
-x = Node(10)
-my_list.head = x
-print(my_list.print_all_list())
+first = Node(10)
+second  = Node(20)
+third = Node(30)
+my_list.head = first
+first.next  = second
+second.next = third
+
+
+
+
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+      
+        return len(self.items) == 0
+
+    def enqueue(self, item):
+
+        self.items.append(item)
+
+
+    def dequeue(self):
+        if self.is_empty():
+            return None
+        return self.items.pop(0)
+
+    def size(self):
+        return len(self.items)
+
+    def print_all(self):
+        print( self.items)
+
+
+
+
+class Stack:
+    def __init__(self):
+        self.stack = []
+        
+    def push(self,new_item):
+        self.stack.append(new_item)
+    
+    def pop(self):
+        if not self.is_empty():
+            self.stack.pop()
+            return
+        return -1
+    
+    def is_empty(self):
+        return len(self.stack) == 0
+    
+    def size(self):
+        return len(self.stack)
+    
+    def peek(self):
+        return self.stack[-1]
+    
+    def rest(self):
+        return  Stack
---
+++ service.py
@@ -0,0 +1,13 @@
+from main import Queue , Stack
+
+browser_history = Stack()
+browser_history.push("google.com")
+browser_history.push("github.com")
+browser_history.push("youtube.com")
+
+check_out_queue = Queue()
+
+
+check_out_queue.enqueue("Customer 1")
+check_out_queue.enqueue("Customer 2")
+check_out_queue.enqueue("Customer 2")
